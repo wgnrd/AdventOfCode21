bd8ebf0cab9fff64b0e2adfea6a057dd98c87461
Wagner <dominik.waagna@gmail.com>
2021-12-02 13:40:35 +0100

    star 2

--- 01/main.py
+++ 01/main.py
@@ -1,23 +1,45 @@
+import sys
+
+sys.path.append("../common")
+import utils
+
+
 def read_input_file():
   file = open(".\input.txt", "r")
   content = file.read()
-  content_list = content.splitlines()
+  contentList = content.splitlines()
   file.close()
-  return content_list
+  return list(map(int, contentList))
 
 
-def count_increase(depth_list):
+def count_increase(depthList):
   count = 0
-  for i, j in enumerate(depth_list):
-    if (i > 0 and (depth_list[i] > depth_list[i - 1])):
+  for i, j in enumerate(depthList):
+    if (i > 0 and (depthList[i] > depthList[i - 1])):
       count += 1
 
   return count
 
 
+def split_windows(depthList):
+  windowList = []
+
+  for i, j in enumerate(depthList):
+    if ((i + 2) < len(depthList)):
+      windowList.append(depthList[i] + depthList[i + 1] + depthList[i + 2])
+
+  return windowList
+
+
 def main():
-  depth_list = read_input_file()
-  print(count_increase(depth_list))
+  utils.displayHeaderForDay(1)
+  # Star 1
+  depthList = read_input_file()
+  print(f'Result Star 1: {count_increase(depthList)}')
+
+  # Star 2
+  windowList = split_windows(depthList)
+  print(f'Result Star 2: {count_increase(windowList)}')
 
 
 main()
