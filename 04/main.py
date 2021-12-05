95a7131026d35072d0ef8d8be5cdf7b2de1c0f88
Wagner <dominik.waagna@gmail.com>
2021-12-05 20:26:59 +0100

    part 0

---
+++ 04/main.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.path.append("../common")
+import utils
+
+
+def main():
+  utils.display_header_for_day(4)
+  # Star 1
+  print(f'Result Star 1: {0}')
+
+  # Star 2
+  # print(f'Result Star 2: {}')
+
+
+main()
