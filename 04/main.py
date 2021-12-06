90d4f656422c887e30023eb5d8d1ef5572489fb7
Wagner <dominik.waagna@gmail.com>
2021-12-06 10:07:19 +0100

    add functionality

--- 04/main.py
+++ 04/main.py
@@ -8,21 +8,68 @@ import utils
 class BingoInput:
   drawnNumbers: List[int] = []
   boards: List[int] = []
+  markings: List[bool] = []
 
   def parse_input(self):
     content = utils.read_input_file()
-    foo = content.split('\n\n')
-    self.drawnNumbers = foo[0].split(',')
+    tmpSplit = content.split('\n\n')
+    self.drawnNumbers = tmpSplit[0].split(',')
 
-    del foo[0]
+    del tmpSplit[0]
 
-    for board in foo:
+    for board in tmpSplit:
       tmpBoard: List[int] = []
+      self.markings.append([[False] * 5] * 5)
       for line in board.split('\n'):
         bar = list(filter(None, line.split(' ')))
-        tmpBoard.extend(bar)
+        tmpBoard.append(bar)
 
       self.boards.append(tmpBoard)
+
+  def is_winning_board(self, markings: List[int]) -> bool:
+    for i in range(len(markings)):
+      if len(markings[i]) == len([t for t in markings[i] if t == True]):
+        return True
+
+    for i in range(len(markings[i])):
+      col = []
+      for j in range(len(markings)):
+        col.append(markings[i][j])
+      if len(col) == len([t for t in col if t == True]):
+        return True
+
+    return False
+
+  def calculate_score(self, board: List[int], markings: List[int]) -> int:
+    score = 0
+    for i in range(len(board)):
+      for j in range(len(board[i])):
+        if not markings[i][j]:
+          score += board[i][j]
+
+    return score
+
+  # performs the board markings for a drawn number
+  def draw(self, drawnNumber) -> int:
+    for idx, board in enumerate(self.boards):
+      for i in range(len(board)):
+        for j in range(len(board[i])):
+          if board[i][j] == drawnNumber:
+            # ERROR: markings affect whole column instead of cell
+            print(idx, i, j, drawnNumber)
+            self.markings[idx][i][j] = True
+            print(self.markings[idx][i])
+            print(self.markings[idx])
+
+      if self.is_winning_board(self.markings[idx]):
+        return self.calculate_score(self.boards[idx], self.markings[idx])
+
+  # finds first winner and returns the score
+  def find_first_winner(self) -> int:
+    for drawnNumber in self.drawnNumbers:
+      score = self.draw(drawnNumber)
+      if score != None:
+        return score
 
 
 def main():
@@ -30,9 +77,8 @@ def main():
 
   bi = BingoInput()
   bi.parse_input()
-  print(bi.drawnNumbers)
   # Star 1
-  print(f'Result Star 1: {0}')
+  print(f'Result Star 1: {bi.find_first_winner()}')
 
   # Star 2
   # print(f'Result Star 2: {}')
